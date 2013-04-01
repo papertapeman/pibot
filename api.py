84bf64bc1592fda136eb47761f2ab3da12611767
Adam Piper <adam@ahri.net>
2013-04-01 12:39:38 +0100

    Iron out basic tests.

--- api.py
+++ api.py
@@ -3,7 +3,7 @@
 from abc import ABCMeta, abstractmethod
 
 
-class Bus(object):
+class ABus(object):
 
     __metaclass__ = ABCMeta
 
@@ -16,11 +16,24 @@ class Bus(object):
         "Write a byte to the given address."
 
 
-class BusAddress(object):
+class ABusAddress(object):
 
     """
-    Perform operations on a bus address.
+    Perform operations on a defined bus address.
     """
+
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def read_byte(self):
+        pass
+
+    @abstractmethod
+    def write_byte(self, byte):
+        pass
+
+
+class BusAddress(object):
 
     def __init__(self, bus, address):
         self.bus = bus
@@ -31,3 +44,20 @@ class BusAddress(object):
 
     def write_byte(self, byte):
         self.bus.write_byte(self.address, byte)
+
+
+class BusAddressExecuter(object):
+
+    def __init__(self, bus_address, execution_leader):
+        self.bus_address = bus_address
+        self.execution_leader = execution_leader
+
+    def execute(self, command, *params):
+        self.bus_address.write_byte(self.execution_leader)
+        self.bus_address.write_byte(command)
+        for param in params:
+            self.bus_address.write_byte(param)
+
+
+# Registrations
+ABusAddress.register(BusAddress)
--- tests.py
+++ tests.py
@@ -2,49 +2,68 @@
 # coding: utf-8
 
 from unittest import TestCase, main as test_main
-from doublex.pyDoubles import mock, expect_call
+from doublex import Mock, verify
 from hamcrest import assert_that, equal_to
-from api import Bus, BusAddress
-
-
-# from http://stackoverflow.com/a/9759329
-def concreter(abclass):
-    """
-    >>> c = concreter(Abstract)
-    >>> c.__name__
-    'dummy_concrete_Abstract'
-    >>> c().bar()
-    bar
-    """
-    if not "__abstractmethods__" in abclass.__dict__:
-        return abclass
-    new_dict = abclass.__dict__.copy()
-    for abstractmethod in abclass.__dict__["__abstractmethods__"]:
-        #replace each abc method or property with an identity function:
-        new_dict[abstractmethod] = lambda x, *args, **kw: (x, args, kw)
-    #creates a new class, with the overriden ABCs:
-    return type("dummy_concrete_%s" % abclass.__name__, (abclass,), new_dict)
+from api import ABus, ABusAddress, BusAddress, BusAddressExecuter
 
 
 class TestBusAddress(TestCase):
 
     def setUp(self):
-        self.mock_bus = mock(concreter(Bus))
         self.address = 0x00
-        self.sut = BusAddress(self.mock_bus, self.address)
 
     def test_read_byte(self):
+        # Arrange
         BYTE_TO_READ = 0x00
-        expect_call(self.mock_bus.read_byte).with_args(self.address).returning(BYTE_TO_READ)
-        assert_that(self.sut.read_byte(),
+        with Mock(ABus) as mock_bus:
+            mock_bus.read_byte(self.address).returns(BYTE_TO_READ)
+
+        # Act
+        bus_address = BusAddress(mock_bus, self.address)
+
+        # Assert
+        assert_that(bus_address.read_byte(),
                     equal_to(BYTE_TO_READ))
-        self.mock_bus.assert_that_is_satisfied()
+
+        assert_that(mock_bus, verify())
 
     def test_write_byte(self):
+        # Arrange
         BYTE_TO_WRITE = 0x00
-        expect_call(self.mock_bus.write_byte).with_args(self.address, BYTE_TO_WRITE)
-        self.sut.write_byte(BYTE_TO_WRITE)
-        self.mock_bus.assert_that_is_satisfied()
+        with Mock(ABus) as mock_bus:
+            mock_bus.write_byte(self.address, BYTE_TO_WRITE)
+
+        bus_address = BusAddress(mock_bus, self.address)
+
+        # Act
+        bus_address.write_byte(BYTE_TO_WRITE)
+
+        # Assert
+        assert_that(mock_bus, verify())
+
+
+class TestBusExecute(TestCase):
+
+    def setUp(self):
+        self.execution_leader = "exec"
+
+    def test_execute(self):
+        # Arrange
+        COMMAND = "cmd"
+        PARAMS = ("p1", "p2")
+        with Mock(ABusAddress) as mock_bus_address:
+            mock_bus_address.write_byte(self.execution_leader)
+            mock_bus_address.write_byte(COMMAND)
+            for param in PARAMS:
+                mock_bus_address.write_byte(param)
+
+        bus_address_executer = BusAddressExecuter(mock_bus_address, self.execution_leader)
+
+        # Act
+        bus_address_executer.execute(COMMAND, *PARAMS)
+
+        # Assert
+        assert_that(mock_bus_address, verify())
 
 if __name__ == '__main__':
     test_main()
